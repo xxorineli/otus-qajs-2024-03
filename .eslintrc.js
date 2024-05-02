7eb79f7949ece4bdebeb914d192bab0d5acd7866
Eli Zorin <xxorineli@gmail.com>
2024-05-02 09:37:41 +0300

    upd env and new factorial files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: ['standard', 'prettier'],
   overrides: [
@@ -19,5 +20,21 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {},
+  plugins: ['jest'],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  settings: {
+    jest: {
+      globalAliases: {
+        describe: ['context'],
+        fdescribe: ['fcontext'],
+        xdescribe: ['xcontext'],
+      },
+    },
+  },
 }
